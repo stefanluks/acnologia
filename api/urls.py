a15fb13178ad5353a307a939678bb2384ba92daf
stefanluks <stefan.luks@rede.ulbra.br>
2025-02-23 23:57:53 -0300

    add GetLikes fviews

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from .viewsets import UsuarioViewSet, JogoViewSet
+from .views import GetQtdLikes, GetLikes
 
 rotas = routers.DefaultRouter()
 rotas.register(r'usuarios', UsuarioViewSet)
@@ -9,5 +10,7 @@ rotas.register(r'jogos', JogoViewSet)
 
 urlpatterns = [
     path("", include(rotas.urls)),
+    path('GetQtdLikes/<int:id_jogo>/', GetQtdLikes, name="GetQtdLikes"),
+    path('GetLikes/<int:id_jogo>/', GetLikes, name="GetLikes"),
     path('api-rest/', include('rest_framework.urls')),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
+from .models import Jogo, Like, Seguidor, Usuario
 
 # Create your views here.
 def Main(request):
     return JsonResponse(data={"erro":False}, safe=False)
+
+def GetQtdLikes(request, id_jogo):
+    if Jogo.objects.filter(pk=id_jogo):
+        jogo = Jogo.objects.get(pk=id_jogo)
+        cont = len(list(Like.objects.filter(pk=id_jogo)))
+        return JsonResponse(data={
+            "error":False,
+            "jogo":jogo.pk,
+            "nome_jogo":jogo.nome,
+            "qtd_likes":cont,
+        })
+    else:
+        return JsonResponse(data={
+            "error":True,
+            "msg":"Jogo não encontrado!"
+        })
+
+
+def GetLikes(request, id_jogo):
+    if Jogo.objects.filter(pk=id_jogo):
+        jogo = Jogo.objects.get(pk=id_jogo)
+        cont = len(list(Like.objects.filter(pk=id_jogo)))
+        obj = {
+            "error":False,
+            "jogo":jogo.pk,
+            "nome_jogo":jogo.nome,
+            "qtd_likes":cont,
+            "quem_curtiu":[]
+        }
+        for l in list(Like.objects.filter(pk=id_jogo)):
+            json = {"id":l.id, "usuario":{"id":l.usuario.id, "nome":l.usuario.nome}}
+            obj["quem_curtiu"].append(json)
+        return JsonResponse(data=obj, safe=False)
+    else:
+        return JsonResponse(data={
+            "error":True,
+            "msg":"Jogo não encontrado!"
+        }, safe=False)
